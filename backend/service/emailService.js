91a7fc17d4d9d55b90b5e465c7615783d1ffadb8
Rishee Shukla <risheeshukla0@gmail.com>
2026-06-18 18:49:34 +0530

    Update email transport configuration to use port 587

--- backend/service/emailService.js
+++ backend/service/emailService.js
@@ -4,8 +4,9 @@ dotenv.config();
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  port: 587,
+  secure: false,
+  family: 4, // force IPv4
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
