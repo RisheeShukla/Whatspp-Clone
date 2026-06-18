91c18648dc6851410310c6cc14db2cf7eab06cc9
Rishee Shukla <risheeshukla0@gmail.com>
2026-06-18 19:06:57 +0530

    Refactor email transport configuration to use Gmail service

--- backend/service/emailService.js
+++ backend/service/emailService.js
@@ -3,10 +3,7 @@ const dotenv=require('dotenv');
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  family: 4, // force IPv4
+  service:"gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
