6b92609bc546b4066b69a446e9002b3d65bfc86e
Rishee Shukla <risheeshukla0@gmail.com>
2026-06-18 18:39:44 +0530

    Change email transport to use SMTP settings
    Updated email service configuration to use SMTP settings.

--- backend/service/emailService.js
+++ backend/service/emailService.js
@@ -3,11 +3,16 @@ const dotenv=require('dotenv');
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  connectionTimeout: 60000, 
+  greetingTimeout: 60000,  
+  socketTimeout: 60000,    
 });
 
 
